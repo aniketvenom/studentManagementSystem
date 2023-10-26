aab91cdf4b0d9b4dfda42e8f9e6c0470f46eb444
aniketvenom <144891476+aniketvenom@users.noreply.github.com>
2023-10-26 23:51:52 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RegistorStudentComponent } from './registor-student/registor-student.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentListComponent,
+    HomeComponent,
+    RegistorStudentComponent,
+   
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
